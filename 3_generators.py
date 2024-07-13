4296c4953930599a687215c087051c598e1ff052
Arthur <temp2003late@gmail.com>
2024-07-13 13:00:26 +0300

    5

--- 3_generators.py
+++ 3_generators.py
@@ -2,11 +2,6 @@ from time import time
 
 # Round Robin
 
-def gen(s):
-    for i in s:
-        yield i
-
-
 def gen_filename():
     while True:
         pattern = 'file-{}.jpeg'
@@ -17,4 +12,34 @@ def gen_filename():
         sum = 234 + 234
         print(sum)
 
-g = gen_filename()
+# g = gen_filename()
+
+def gen(s):
+    for i in s:
+        yield i
+        
+
+def gen1():
+    for i in s:
+        yield i
+
+
+def gen2():
+    for i in range(n):
+        yield n
+
+
+g1 = gen1('artur')
+g2 = gen2(4)
+
+tasks = [g1, g2]
+
+while tasks:
+    task = tasks.pop(0)
+
+    try:
+        i = next(task)
+        print(i)
+        tasks.append(task)
+    except StopIteration:
+        pass
