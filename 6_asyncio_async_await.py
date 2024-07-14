f25d413f5e8e1ea9d67f2f5509ac44831d9e3aff
Arthur <temp2003late@gmail.com>
2024-07-14 13:43:59 +0300

    FINISH README.md

---
+++ 6_asyncio_async_await.py
@@ -0,0 +1,14 @@
+# pip (pip3) install aiogttp
+
+
+# План
+#  1. Asyncio фреймворк для створення подійних циклів
+#  2. Приклад простих асинхронних програм часів Python 3.4
+#  3. Синтаксис Async/await на pfvsnre @asyncio.coroutine і yield from
+#  4. Приклад асинхронного скачування файлів
+
+
+# Event Loop:
+#     coroutine > Task (Future)
+
+
